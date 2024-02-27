cd67ba11ac5e1a1c1d71b7202cd532ca85d9534e
Rom Balax <rbalax@yahoo.co.uk>
2024-02-27 09:07:12 +0900

    initial commit

---
+++ app/controllers/blog_posts_controller.rb
@@ -0,0 +1,58 @@
+class BlogPostsController < ApplicationController
+
+  before_action :set_blog_post, except: [:index, :new, :create]
+
+  def index
+    @blog_posts = BlogPost.all
+  end
+
+  def new
+    @blog_post = BlogPost.new
+  end
+
+
+  def create
+    @blog_post = BlogPost.new(blog_post_params)
+    if @blog_post.save
+    redirect_to @blog_post
+    else
+    render :new, status: :unprocessable_entity
+    flash.now[:error] = "Invalid entry"
+    end
+  end
+
+  def show
+    @blog_post = BlogPost.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to root_path
+  end
+
+  def edit
+  end
+
+
+  def update
+    if @blog_post.update(blog_post_params)
+      puts blog_post_params[:body]
+
+    redirect_to @blog_post, flash: { success: "Blog post was successfully updated." }
+  else
+    render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @blog_post.destroy
+    redirect_to blog_posts_path, flash: { success: "Blog post was successfully deleted." }
+  end
+
+  private
+
+  def blog_post_params
+    params.require(:blog_post).permit(:title, :body)
+  end
+
+  def set_blog_post
+    @blog_post = BlogPost.find(params[:id])
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,38 @@
+Rails.application.routes.draw do
+
+  resources :blog_posts
+
+  root to: 'blog_posts#index'
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  #get "up" => "rails/health#show", as: :rails_health_check
+
+  # get 'blog_posts', to: 'blog_posts#index'
+  # get 'blog_posts/new', to: 'blog_posts#new', as: 'new_blog_post'
+
+
+  # post 'blog_posts', to: 'blog_posts#create'
+  # patch 'blog_posts/:id', to: 'blog_posts#update'
+
+  # get 'blog_posts/:id/edit', to: 'blog_posts#edit', as: 'edit_blog_post'
+
+
+  # get 'blog_posts/:id', to: 'blog_posts#show', as: 'blog_post'
+
+  # delete 'blog_posts/:id', to: 'blog_posts#destroy'
+
+  # Route for listing all blog posts
+
+  # Route for creating a new blog post
+
+  # Route for displaying the form to create a new blog post
+
+  # Route for displaying the form to edit an existing blog post
+
+  # Route for showing a specific blog post
+
+
+end

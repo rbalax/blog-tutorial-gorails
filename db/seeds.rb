e78effc9192bfbfa61095cea035c1bcc017edd5e
Rom Balax <rbalax@yahoo.co.uk>
2024-03-04 12:32:16 +0900

    s3 again

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,10 @@ require 'faker'
 20.times do |n|
   case n % 3
   when 0
-    BlogPost.create(title: Faker::Book.title, body: Faker::Lorem.paragraphs(number: 3).join("\n\n"), published_at: nil)
+    BlogPost.create(title: Faker::Book.title, content: Faker::Lorem.paragraphs(number: 3).join("\n\n"), published_at: nil)
   when 1
-    BlogPost.create(title: Faker::Book.title, body: Faker::Lorem.paragraphs(number: 3).join("\n\n"), published_at: Time.current - rand(1..365).days)
+    BlogPost.create(title: Faker::Book.title, content: Faker::Lorem.paragraphs(number: 3).join("\n\n"), published_at: Time.current - rand(1..365).days)
   when 2
-    BlogPost.create(title: Faker::Book.title, body: Faker::Lorem.paragraphs(number: 3).join("\n\n"), published_at: Time.current + rand(1..365).days)
+    BlogPost.create(title: Faker::Book.title, content: Faker::Lorem.paragraphs(number: 3).join("\n\n"), published_at: Time.current + rand(1..365).days)
   end
 end
